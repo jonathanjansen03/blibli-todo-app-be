523eb9f4030d5bd513bb2295c06bf716a05e9288
WinstarWyvern <winston.kusumo@gdn-commerce.com>
2023-03-15 14:00:50 +0700

    - Add CRUD in TransactionController and TransactionService - Setup Local DB

--- src/main/java/com/example/bliblitokobukuappbe/controllers/TransactionController.java
+++ src/main/java/com/example/bliblitokobukuappbe/controllers/TransactionController.java
@@ -1,9 +1,12 @@
 package com.example.bliblitokobukuappbe.controllers;
 
+import com.example.bliblitokobukuappbe.pojos.Transaction;
 import com.example.bliblitokobukuappbe.services.TransactionService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -11,6 +14,37 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransactionController {
 
     private TransactionService transactionService;
+
+    public List<Transaction> getTransactions() {
+        return transactionService.getTransactions();
+    }
+
+    @PostMapping(
+            path = "/insert",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public void insertTransaction(@RequestBody Transaction transaction) {
+        transactionService.insertTransaction(transaction);
+    }
+
+    @PutMapping(
+            path = "/update/{transactionId}",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public void updateTransaction(@RequestBody Transaction transaction, @PathVariable String transactionId) {
+        transactionService.updateTransaction(transaction, transactionId);
+    }
+
+    @DeleteMapping(
+            path = "/delete/{transactionId}",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public void deleteTransaction(@PathVariable String transactionId) {
+        transactionService.deleteTransaction(transactionId);
+    }
 
 
 }
--- src/main/java/com/example/bliblitokobukuappbe/services/TransactionService.java
+++ src/main/java/com/example/bliblitokobukuappbe/services/TransactionService.java
@@ -1,13 +1,33 @@
 package com.example.bliblitokobukuappbe.services;
 
+import com.example.bliblitokobukuappbe.pojos.Transaction;
 import com.example.bliblitokobukuappbe.repositories.TransactionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class TransactionService {
 
     private final TransactionRepository transactionRepository;
+
+    public List<Transaction> getTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public void insertTransaction(Transaction newTransaction) {
+        transactionRepository.save(newTransaction);
+    }
+
+    public void updateTransaction(Transaction newTransaction, String id) {
+        transactionRepository.findById(id).ifPresent(oldTransaction -> transactionRepository.save(newTransaction));
+
+    }
+
+    public void deleteTransaction(String id) {
+        transactionRepository.deleteById(id);
+    }
 
 }
